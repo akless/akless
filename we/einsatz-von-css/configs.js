db016d3b31507ad77cbf0a72ae167820a7856061
Andre Kless <andre.kless@inf.h-brs.de>
2017-06-27 23:29:44 +0200

    reorganization of learning unit "Einsatz von CSS"

---
+++ we/einsatz-von-css/configs.js
@@ -0,0 +1,11 @@
+/**
+ * @overview ccm instance configurations
+ * @author André Kless <andre.kless@web.de> 2017
+ * @license The MIT License (MIT)
+ */
+ccm.files[ "configs.min.js" ] = {
+  "le": {
+    "topic": "Einsatz von CSS",
+    "innerHTML": ""
+  }
+};
