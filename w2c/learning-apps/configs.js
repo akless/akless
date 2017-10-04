675789f869750f0581693fbea1dcf59d04fbc2c0
Andre Kless <andre.kless@inf.h-brs.de>
2017-10-04 21:04:30 +0200

    update Slidecast for LearningApps

--- w2c/learning-apps/configs.js
+++ w2c/learning-apps/configs.js
@@ -8,8 +8,8 @@ ccm.files[ 'configs.js' ] = {
   "slidecast": {
     "slides": [
       {
-        "image": "resources/Folie01.jpg",
-        "audio": "resources/jingle.mp3",
+        "image": "https://akless.github.io/akless/w2c/learning-apps/resources/Folie01.jpg",
+        "audio": "https://akless.github.io/akless/w2c/learning-apps/resources/jingle.mp3",
         "description": "Zu jeder Folie kann eine Beschreibung mit zusätzlichen Erläuterungen des Lehrenden hinterlegt werden.",
         "optional_content": [ "ccm.instance", "https://tkless.github.io/ccm-components/comment/versions/ccm.comment-1.0.0.min.js", {
           "comment_template": "expanded",
@@ -28,33 +28,33 @@ ccm.files[ 'configs.js' ] = {
         }]
       },
       {
-        "image": "resources/Folie02.jpg",
+        "image": "https://akless.github.io/akless/w2c/learning-apps/resources/Folie02.jpg",
         "optional_content": [ "ccm.instance", "https://akless.github.io/ccm-components/quiz/versions/ccm.quiz-2.0.0.min.js", [ "ccm.get", "https://akless.github.io/ccm-components/quiz/resources/configs.js", "demo" ] ]
       },
       {
-        "image": "resources/Folie03.jpg",
+        "image": "https://akless.github.io/akless/w2c/learning-apps/resources/Folie03.jpg",
         "optional_content": [ "ccm.instance", "https://akless.github.io/ccm-components/cloze/versions/ccm.cloze-2.0.0.min.js", [ "ccm.get", "https://akless.github.io/ccm-components/cloze/resources/configs.js", "demo" ] ]
       },
       {
-        "image": "resources/Folie04.jpg"
+        "image": "https://akless.github.io/akless/w2c/learning-apps/resources/Folie04.jpg"
 //      "optional_content": [ "ccm.instance", "https://akless.github.io/ccm-components/kanban_board/ccm.kanban_board.min.js", [ "ccm.get", "https://akless.github.io/ccm-components/kanban_board/resources/configs.js", "demo" ] ]
       },
       {
-        "image": "resources/Folie05.jpg",
+        "image": "https://akless.github.io/akless/w2c/learning-apps/resources/Folie05.jpg",
         "optional_content": [ "ccm.instance", "https://akless.github.io/ccm-components/teambuild/versions/ccm.teambuild-1.0.0.min.js", [ "ccm.get", "https://akless.github.io/ccm-components/teambuild/resources/configs.js", "demo" ] ]
       },
       {
-        "image": "resources/Folie06.jpg"
+        "image": "https://akless.github.io/akless/w2c/learning-apps/resources/Folie06.jpg"
       },
       {
-        "image": "resources/Folie07.jpg",
-        "audio": "resources/jingle.mp3  "
+        "image": "https://akless.github.io/akless/w2c/learning-apps/resources/Folie07.jpg",
+        "audio": "https://akless.github.io/akless/w2c/learning-apps/resources/jingle.mp3  "
       },
       {
-        "image": "resources/Folie08.jpg"
+        "image": "https://akless.github.io/akless/w2c/learning-apps/resources/Folie08.jpg"
       },
       {
-        "image": "resources/Folie09.jpg"
+        "image": "https://akless.github.io/akless/w2c/learning-apps/resources/Folie09.jpg"
       }
     ]
   }
