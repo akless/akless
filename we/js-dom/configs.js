1850c44e264be5d38c9c38e5615b6d691ac1477f
Andre Kless <andre.kless@inf.h-brs.de>
2018-01-19 21:38:35 +0100

    add learning unit "Das DOM - Mysterium der JavaScript-Welt"

---
+++ we/js-dom/configs.js
@@ -0,0 +1,16 @@
+/**
+ * @overview ccm instance configurations
+ * @author André Kless <andre.kless@web.de> 2018
+ * @license The MIT License (MIT)
+ */
+ccm.files[ "configs.js" ] = {
+  "le": {
+    "css": [ "ccm.load", "https://akless.github.io/ccm-components/le/resources/weblysleek.css", { "context": "head", "url": "https://akless.github.io/ccm-components/libs/weblysleekui/font.css" } ],
+    "logo": "https://akless.github.io/akless/we/logo.png",
+    "topic_prefix": "Lerneinheit:",
+    "topic": "Das DOM - Mysterium der JavaScript-Welt",
+    "link_prefix": "Link:",
+    "link_target": "_blank",
+    "author": "André Kless"
+  }
+};
